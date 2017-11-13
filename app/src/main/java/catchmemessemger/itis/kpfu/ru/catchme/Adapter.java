bce0dbeeb4d3e5aae7595d58d46d0118c5cdc119
Lesya <sikamikanico9494@gmail.com>
2017-11-13 19:22:18 +0300

    duplication of chats fixed

---
+++ app/src/main/java/catchmemessemger/itis/kpfu/ru/catchme/Adapter.java
@@ -0,0 +1,57 @@
+package catchmemessemger.itis.kpfu.ru.catchme;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class Adapter extends BaseAdapter {
+
+    private Context mContext;
+    private String [] users;
+
+    public Adapter(Context context, String[] users) {
+        mContext = context;
+        this.users = users;
+    }
+
+    static class ViewHolder {
+        TextView userName;
+    }
+
+    @Override
+    public int getCount() {
+        return users.length;
+    }
+
+    @Override
+    public String getItem(int i) {
+        return users[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        ViewHolder viewHolderName;
+        if (convertView == null){
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.chat_list_item, viewGroup, false);
+            viewHolderName = new ViewHolder();
+            viewHolderName.userName = (TextView) convertView.findViewById(R.id.userName);
+            convertView.setTag(viewHolderName);
+        } else {
+            viewHolderName = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolderName.userName.setText(getItem(position));
+
+        return convertView;
+    }
+}
--- app/src/main/java/catchmemessemger/itis/kpfu/ru/catchme/ChatListActivity.java
+++ app/src/main/java/catchmemessemger/itis/kpfu/ru/catchme/ChatListActivity.java
@@ -6,65 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
 
 public class ChatListActivity extends AppCompatActivity {
-    final String ATTRIBUTE_NAME_USER = "user";
-    final String ATTRIBUTE_NAME_MESSAGE = "message";
-    final String ATTRIBUTE_NAME_TIME = "time";
-    public static ArrayList users = new ArrayList();
-    public static ArrayList lastMessages = new ArrayList();
-    public static ArrayList time = new ArrayList();
+
+    public static String [] users = new String[3];
+    public int iaddChat = 0;
     public void addChat(String chat) {
-        users.add(chat);
+        users[iaddChat] = chat;
+        iaddChat++;
     }
 
-    public void addLastMessage(String message) {
-        lastMessages.add(message);
-    }
 
-    public void addTime(String time) {
-        this.time.add(time);
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_list);
-        //TODO getting chats info
         addChat("User1");
         addChat("User2");
         addChat("User3");
 
-        addLastMessage("Hey");
-        addLastMessage("Hello");
-        addLastMessage("Bye");
 
-        addTime("20:00");
-        addTime("22:22");
-        addTime("10:15");
-
-        ArrayList<Map<String, Object>> data = new ArrayList<Map<String, Object>>(users.size());
-        Map<String, Object> m;
-        for (int i = 0; i < users.size(); i++) {
-            m = new HashMap<String, Object>();
-            m.put(ATTRIBUTE_NAME_USER, users.get(i));
-            m.put(ATTRIBUTE_NAME_MESSAGE, lastMessages.get(i));
-            m.put(ATTRIBUTE_NAME_TIME, time.get(i));
-            data.add(m);
-        }
-
-        String[] from = {ATTRIBUTE_NAME_USER, ATTRIBUTE_NAME_MESSAGE, ATTRIBUTE_NAME_TIME};
-        int [] to = {R.id.userName, R.id.lastMassage, R.id.time};
-
-        SimpleAdapter sAdapter = new SimpleAdapter(this, data, R.layout.chat_list_item, from, to);
+        Adapter adapter = new Adapter(this, users);
 
         ListView lv = (ListView) findViewById(R.id.lv);
-        lv.setAdapter(sAdapter);
+        lv.setAdapter(adapter);
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
 
@@ -77,4 +42,5 @@ public class ChatListActivity extends AppCompatActivity {
 
 
     }
+
 }
