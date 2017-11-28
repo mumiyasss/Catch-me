a657c0682bf6dff36ffe66e7a68dc0c046111bc3
Lesya <sikamikanico9494@gmail.com>
2017-11-28 18:05:22 +0300

    Messages added to Chat Activity

---
+++ app/src/main/java/catchme/messenger/catchme/Adapter.java
@@ -0,0 +1,57 @@
+package catchme.messenger.catchme;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class Adapter extends BaseAdapter {
+
+    private Context mContext;
+    private String [] messages;
+
+    public Adapter(Context context, String[] messagese) {
+        mContext = context;
+        this.messages = messages;
+    }
+
+    static class ViewHolder {
+        TextView messageText;
+    }
+
+    @Override
+    public int getCount() {
+        return messages.length;
+    }
+
+    @Override
+    public String getItem(int i) {
+        return messages[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        ViewHolder viewHolderName;
+        if (convertView == null){
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.message, viewGroup, false);
+            viewHolderName = new ViewHolder();
+            viewHolderName.messageText = (TextView) convertView.findViewById(R.id.messageText);
+            convertView.setTag(viewHolderName);
+        } else {
+            viewHolderName = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolderName.messageText.setText(getItem(position));
+
+        return convertView;
+    }
+
+}
--- app/src/main/java/catchme/messenger/catchme/ChatActivity.java
+++ app/src/main/java/catchme/messenger/catchme/ChatActivity.java
@@ -2,14 +2,29 @@ package catchme.messenger.catchme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 public class ChatActivity extends AppCompatActivity {
+    static String [] messages = new String[100];
+    static int size = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        messages[0] = "hello";
+        messages[1] = "its meee";
+        size += 2;
+        Adapter adapter = new Adapter(this, messages);
+        ListView lv = (ListView) findViewById(R.id.chatListView);
+        lv.setAdapter(adapter);
 
+    }
+
+    public void send(View view) {
+        messages[size] = "This is some message";
+        size++;
     }
 
 
