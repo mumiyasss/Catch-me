2344f7f819e143098a77711fedf2066d2ff5f351
Kolya Grebnev <mumiyasss@gmail.com>
2017-12-10 09:37:22 +0300

    Server connection established

---
+++ app/src/main/java/catchme/messenger/logic/STUDY.java
@@ -0,0 +1,13 @@
+package catchme.messenger.catchme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class STUDY extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study);
+    }
+}
