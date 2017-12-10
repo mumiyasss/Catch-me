708a5186a5aeef220c2eaee4553a3c26c157e5a8
Kolya Grebnev <mumiyasss@gmail.com>
2017-12-10 09:37:37 +0300

    Server connection established

--- app/src/main/java/catchme/messenger/logic/LogicTest.java
+++ app/src/main/java/catchme/messenger/logic/LogicTest.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 
+import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
@@ -34,98 +35,23 @@ public class LogicTest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logic_test);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
         retrofit = new Retrofit.Builder()
                 .baseUrl("http://ksftx.pythonanywhere.com/")
                 .build();
         service = retrofit.create(ServerApiInterface.class);
+        //List<Chat> chat = service.getChats();
+        //Log.d("CHAT", chat.toString());
     }
-
-    public void btnPressed (View view) {
-        EditText nameField = findViewById(R.id.name);
-        String name = nameField.getText().toString();
-
-        EditText emailField = findViewById(R.id.email);
-        String email = emailField.getText().toString();
-
-        EditText passwordField = findViewById(R.id.password);
-        String pasword = passwordField.getText().toString();
-
-        EditText messageField = findViewById(R.id.message);
-        String message = messageField.getText().toString();
-    }
-
-    public void refresh() {
-        account.setToken(
-                service.getToken(account)
-        );
-        Log.d("logic", "refresh");
-        System.out.println("logic" + "refresh");
-    }
-
-    /**
-     * METHODS TO USE
-     *///:~
-    // !!! NOT RELATED TO logic.LogicTest CLASS !!!
-    // MUST BE LOGIN ACTIVITY's MEMBERS
+/*
     public void login(View view) {
         this.account = new Account(
                 ((EditText) findViewById(R.id.name)).getText().toString(),
                 ((EditText) findViewById(R.id.password)).getText().toString()
-//                ((EditText) findViewById(R.id.email)).getText().toString()
         );
 
-        String token = service.getToken(this.account);
+        String token = service.getToken(this.account).toString();
         Log.d("logic", "onLogin");
         System.out.println("logic" + "onLogin");
-    }
-
-    public void signup(View view) {
-        this.account = new Account(
-                ((EditText) findViewById(R.id.name)).getText().toString(),
-                ((EditText) findViewById(R.id.password)).getText().toString()
-//                ((EditText) findViewById(R.id.email)).getText().toString()
-        );
-
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                service.register(account);
-            }
-        });
-        thread.start();
-
-        this.login(view);
-        Log.d("logic", "onSignup");
-        System.out.println("logic" + "onSignup");
-    }
-
-    public void logout(View view) {
-        Log.d("logic", "onLogout");
-        System.out.println("logic" + "logout");
-    }
-
-    /* METHODS RELATED TO MESSAGES *///:~
-    public String getMessages(Integer chatId) throws IOException {
-        Response response
-                = service.getMessages(chatId, account.getToken()).execute();
-        return response.body().toString();
-    }
-
-    public void sendMessage(final Message message) {
-        final Account account = this.account;
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                service.sendMessage(account.getToken(), message);
-            }
-        });
-        thread.start();
-    }
-
-
-
-
+    }*/
 }
