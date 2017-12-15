d5f79f09cff228f2d2434b409269706c4d0625be
Kolya Grebnev <mumiyasss@gmail.com>
2017-12-15 19:41:50 +0300

    Sending messages

--- app/src/main/java/catchme/messenger/logic/API.java
+++ app/src/main/java/catchme/messenger/logic/API.java
@@ -14,6 +14,7 @@ import catchme.messenger.catchme.R;
 import catchme.messenger.logic.models.Account;
 import catchme.messenger.logic.models.Chat;
 import catchme.messenger.logic.models.Message;
+import catchme.messenger.logic.models.SendingMessage;
 import catchme.messenger.logic.models.Token;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -132,7 +133,6 @@ public class API {
         return chats;
     }
 
-    List<Message> messages;
     void getSyncMessages(final Integer chatId ) {
         Thread th = new Thread(new Runnable() {
             @Override
@@ -157,12 +157,14 @@ public class API {
         Log.d("IN MAIN Thread", messages.toString());
     }
 
+    public List<Message> messages = new ArrayList<>();;
+
     public List<Message> getChatMessages(Integer chatId) {
-        messages = new ArrayList<>();
 
         service.getMessages(chatId, token.getToken()).enqueue(new Callback<List<Message>>() {
             @Override
             public void onResponse(Call<List<Message>> call, Response<List<Message>> response) {
+                messages.clear();
                 messages.addAll(response.body());
                 Log.d("Response", messages.toString());
             }
@@ -173,9 +175,23 @@ public class API {
             }
         });
 
-        Log.d("Log", "getChatMessages() successful call");
-
         return messages;
+    }
+
+    public void sendMessage(Integer chatId, String message) {
+        final SendingMessage sm = new SendingMessage(message, chatId);
+        String conType = "application/json";
+        service.sendMessage(conType, token.getToken(), sm).enqueue(new Callback<Message>() {
+            @Override
+            public void onResponse(Call<Message> call, Response<Message> response) {
+                Log.d("sm", "Message sent");
+            }
+
+            @Override
+            public void onFailure(Call<Message> call, Throwable t) {
+
+            }
+        });
     }
 }
 
